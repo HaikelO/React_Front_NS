8b8e45b70b631d95f8bb0771a0da38156157d7ca
Haikel O <Haikel.ben@gmail.com>
2018-04-10 21:28:37 +0200

    Update

--- src/products/reducers/reducer_products.js
+++ src/products/reducers/reducer_products.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
-import { FETCH_PRODUCTS } from './../actions/index'
+import { FETCH_PRODUCTS, DELETE_PRODUCT } from './../actions/index'
 
 export default function (state = [], action) {
     switch (action.type) {
         case FETCH_PRODUCTS:
             return action.payload.data.SUCCESS;
+        case DELETE_PRODUCT:
+            return _.remove(state, (n) => n._id == action.payload);
         default:
             return state;
     }
